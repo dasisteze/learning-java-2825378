dcf0a0ac6c7df8528bc8e78d003c8a7db23dfd80
Ezequiel Jaime Bencomo <ezequiel.jb7@outlook.com>
2022-04-05 08:42:55 -0500

    init commit, testing git push from intelliJ

---
+++ src/ChallengeOne.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ChallengeOne {
+    public static void main(String[] args) {
+        String question = "What is 10 divided by 2?";
+        String choiceOne = "3";
+        String choiceTwo = "5";
+        String choiceThree = "Invalid, not divisble";
+
+        String correctAnswer = choiceTwo;
+
+        // Write a print statement asking the question
+        // Write a print statement giving the answer choices
+        System.out.println(question);
+        System.out.println(choiceOne + "\n" + choiceTwo + "\n" + choiceThree);
+
+        // Have the user input an answer
+        // Retrieve the user's input
+        System.out.println("Please enter your answer: ");
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+
+        // If the user's input matches the correctAnswer...
+        // then the user is correct and we want to print out a congrats message to the user
+        if (userInput.equals(correctAnswer)) {
+            System.out.println("Congratulations! That is the correct answer!");
+        }
+
+        // If the user's input does not match the correctAnswer...
+        // then the user is incorrect and we want to print out a message saying the user is incorrect
+        else {
+            System.out.println("Sorry! That answer (" + userInput + ") is not correct.");
+        }
+    }
+}
